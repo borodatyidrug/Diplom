8705c91f2a8028d968b7f4d8746499ffe1757afe
borodatyidrug <borodatyidrug@yandex.ru>
2022-05-02 10:37:17 +0300

    Изменена логика работы сервера. Теперь сервер отвечает на запрос и сразу закрывает клиентское соединение, ожидая, при этом, новые соединения на тот же порт. Работу сервера, по-прежнему, можно завершить, но - другой командой - "<shutdown>" - исключительно для удобства. Понятно, что в реальной жизни это - не нужно.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -18,28 +18,38 @@ public class Server {
     }
     
     public void start(int port) {
-        try (ServerSocket serverSocket = new ServerSocket(port);) {
-            
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            
-            System.out.println("Установлено новое соединение.");
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            PrintWriter out;
+            BufferedReader in;
+            Socket clientSocket;
             while (true) {
-                out.flush();
-                out.println("Введите ключевое слово для поиска в формате /{слово} или <end> для завершения сеанса.");
+                clientSocket = serverSocket.accept();
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("Установлено новое соединение.");
+                out.println("Введите ключевое слово для поиска в формате /{слово} или <shutdown> для завершения работы сервера.");
                 String request = in.readLine().trim().split(" ")[0];
-                if (request.equals("<end>")) {
+                if (request.equals("<shutdown>")) {
+                    out.println("Прощайте, Ибрагим-паша из Парги...");
+                    out.close();
+                    in.close();
+                    clientSocket.close();
+                    serverSocket.close();
                     break;
                 } else if (request.startsWith("/{") && request.endsWith("}")) {
                     request = request.substring(2, request.length() - 1);
                     out.println(mapper.writeValueAsString(searchEngine.search(request)));
+                    in.close();
+                    out.close();
+                    clientSocket.close();
                 } else {
                     out.println("Неверный синтаксис запроса.");
+                    in.close();
+                    out.close();
+                    clientSocket.close();
                 }
             }
-            
-            out.println("Прощайте, Ибрагим-паша из Парги...");
             
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
